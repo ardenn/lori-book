cacfed02624284595ab85fd7aa256643f717cfdd
Rodgers Ouma <mc_alila@outlook.com>
2020-11-18 14:37:14 +0300

    Completed story 2

--- tests.py
+++ tests.py
@@ -1,12 +1,22 @@
 from data import Book
-from datetime import datetime, timezone, timedelta
+from datetime import date, timedelta
 
 
 def test_charges1_1_days():
-    book = Book()
+    book = Book(return_date=date.today() + timedelta(days=1))
     assert book.charges1 == 1
 
 
 def test_charges1_0_days():
-    book = Book(return_date=datetime.now(timezone.utc) + timedelta(hours=1))
-    assert book.charges1 == 1
+    book = Book()
+    assert book.charges1 == 0
+
+
+def test_charges2_1_days():
+    book = Book(return_date=date.today() + timedelta(days=1))
+    assert book.charges2 == 1.5
+
+
+def test_charges2_0_days():
+    book = Book()
+    assert book.charges2 == 0
