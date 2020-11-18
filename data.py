0a3043a62df5fcc30417db80d1702d7b984ab74b
Rodgers Ouma <mc_alila@outlook.com>
2020-11-18 14:55:42 +0300

    Completed story 3

--- data.py
+++ data.py
@@ -3,9 +3,9 @@ from datetime import date
 from pydantic import BaseModel
 
 BOOK_TYPES = {
-    "REGULAR": {"charge": 1.5},
+    "REGULAR": {"charge": 1.5, "min_days": 2, "min_charge": 2},
     "FICTION": {"charge": 3},
-    "NOVEL": {"charge": 1.5},
+    "NOVEL": {"charge": 1.5, "min_days": 3, "min_charge": 4.5},
 }
 
 
@@ -38,12 +38,23 @@ class Book(BaseModel):
     def charges2(self):
         return BOOK_TYPES[self.book_type]["charge"] * self.days
 
+    @property
+    def charges3(self):
+        min_days = BOOK_TYPES[self.book_type].get("min_days", 0)
+        min_charge = BOOK_TYPES[self.book_type].get("min_charge", 0)
+        days = self.days
+        if days <= min_days:
+            return min_charge
+        return min_charge + (
+            BOOK_TYPES[self.book_type]["charge"] * (days - min_days)
+        )
+
     def to_dict(self):
         return {
             "book_id": str(self.book_id),
             "book_type": self.book_type,
             "borrow_date": self.borrow_date.isoformat(),
             "return_date": self.return_date.isoformat(),
-            "charges": self.charges2,
+            "charges": self.charges3,
             "days": self.days,
         }
--- tests.py
+++ tests.py
@@ -20,3 +20,31 @@ def test_charges2_1_days():
 def test_charges2_0_days():
     book = Book()
     assert book.charges2 == 0
+
+
+def test_charges3_regular_less_than_min_days():
+    book = Book()
+    assert book.charges3 == 2
+
+
+def test_charges3_regular_equal_to_min_days():
+    book = Book(return_date=date.today() + timedelta(days=2))
+    assert book.charges3 == 2
+
+
+def test_charges3_novel_less_than_min_days():
+    book = Book()
+    book.book_type = "NOVEL"
+    assert book.charges3 == 4.5
+
+
+def test_charges3_novel_greater_than_min_days():
+    book = Book(return_date=date.today() + timedelta(days=4))
+    book.book_type = "NOVEL"
+    assert book.charges3 == 6
+
+
+def test_charges3_fiction_1_days():
+    book = Book(return_date=date.today() + timedelta(days=1))
+    book.book_type = "FICTION"
+    assert book.charges3 == 3
